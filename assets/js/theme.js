61978d01a5829bd027acee730acb646d3608b3e3
L1nSn0w <l1nsn0w@qq.com>
2024-11-10 19:20:05 +0800

    feat: 添加首页样式和主题切换图标

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,8 +2,17 @@ import * as params from '@params';
 
 const comment = params.comment.provider
 const default_theme_config = params.defaulttheme
-const icon_light = params.switch[1]
-const icon_dark = params.switch[0]
+const icon_dark = `
+<svg xmlns="http://www.w3.org/2000/svg" x-bind:width="size" x-bind:height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" x-bind:stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round" width="28" height="28" stroke-width="1.75">
+  <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+  <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
+</svg>
+`
+const icon_light = `
+<svg xmlns="http://www.w3.org/2000/svg" x-bind:width="size" x-bind:height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" x-bind:stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round" width="28" height="28" stroke-width="1.75">
+  <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+</svg>
+`
 const THEME_LIGHT = default_theme_config === 'system' ? 'light' : default_theme_config
 const THEME_DARK = 'dark'
 
